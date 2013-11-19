b58372b9011fd2852c73ca82e54991977f6a2ded
demodawid <demodawid@gmail.com>
2013-11-19 03:08:34 -0300

    Property importante de hibernate, y lazy fetch de festivales

--- workspace/BandasDeRock/src/festival/negocio/model/Festival.java
+++ workspace/BandasDeRock/src/festival/negocio/model/Festival.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -45,7 +46,7 @@ public class Festival implements Serializable {
 	/**
 	 * Las noches del festival
 	 */
-	@OneToMany(cascade=CascadeType.ALL)
+	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.LAZY)
 	@JoinColumn(name="id_festival")
 	private List<Noche> noches;
 	
